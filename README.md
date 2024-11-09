640b014d8ceab99a5a06ac2b8bedb83fc8534bac
michaelzoub <micacao15@gmail.com>
2024-11-09 16:09:38 -0500

    fix

--- README.md
+++ README.md
@@ -1,15 +1,38 @@
-# scrape
+# sv
 
-To install dependencies:
+Everything you need to build a Svelte project, powered by [`sv`](https://github.com/sveltejs/cli).
 
-```bash
-bun install
-```
+## Creating a project
 
-To run:
+If you're seeing this, you've probably already done this step. Congrats!
 
 ```bash
-bun run s
+# create a new project in the current directory
+npx sv create
+
+# create a new project in my-app
+npx sv create my-app
 ```
 
-This project was created using `bun init` in bun v1.1.34. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+## Developing
+
+Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
+
+> To deploy your app, you may need to install an [adapter](https://svelte.dev/docs/kit/adapters) for your target environment.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,8 @@
     import { onMount } from "svelte"
     import { Chart, registerables } from 'chart.js';
 
+    let prod;
+
         Chart.register(...registerables);
 
     let chart;
@@ -43,22 +45,25 @@
                 data: data,
                 options: config
             });
+
+        prod = window.location.href.includes("localhost") ? "http://localhost:8080" : "https://analysooorbackend-production.up.railway.app"
     })
 
-
-    const prod = "https://analysooorbackend-production.up.railway.app/"
     
     let initialWidth = 300
     $: dynamicStyle = `width: ${initialWidth}px;`;
     let isResizing = false;
-    let showModal = "flex flex-col gap-2 w-[400px] h-fit bg-stone-100 mx-auto mt-48 p-6 rounded-lg shadow-2xl";
+    let showModal = "flex flex-col z-50 gap-2 w-[400px] h-fit bg-stone-100 mx-auto mt-48 p-6 rounded-lg shadow-2xl";
     let showLoading = "hidden"
     let showCanvas = "hidden"
 
-    let memeDashboard = "w-full text-sm text-stone-600 transition delay-100 px-4 py-[7px] rounded-lg hover:bg-stone-200 hover:cursor-pointer"
-    let trendDashboard = "w-full text-sm text-stone-600 transition delay-100 0 px-4 py-[7px] rounded-lg hover:bg-stone-200 hover:cursor-pointer"
+    let memeDashboard = "w-full text-sm text-stone-600 transition delay-100 px-4 py-[7px] rounded-lg flex flex-row justify-between hover:bg-stone-200 hover:cursor-pointer"
+    let trendDashboard = "w-full text-sm text-stone-600 transition delay-100 0 px-4 py-[7px] rounded-lg flex flex-row justify-between hover:bg-stone-200 hover:cursor-pointer"
     let errorMessage = "hidden"
     let successMessage = "hidden"
+
+    let wip = "opacity-0 transition delay-300"
+    let noInputValueError = "mx-auto text-stone-100 text-sm"
 
     let inputValue = "";
     let searchValue = "";
@@ -66,7 +71,9 @@
     let meme = "Meme"
     let trend = "Trend"
 
-    let inputFile = "hidden"
+    let addACSVButton = "Add a CSV file"
+
+    let inputFile = "hidden z-50 absolute bg-red-400"
 
 
     let boxChartArray = [0, 0, 0];
@@ -74,9 +81,9 @@
 
     let labelDisplay
 
-	onMount(() => {
-		console.log('the component has mounted');
-	});
+    let fileInput;
+
+    let excelOrNot = false
 
     function startResize(event) {
         isResizing = true
@@ -98,53 +105,39 @@
 
 
 
-    function performUsernameLogic() {
-        const usernameArray = inputValue.split(",")
-        console.log(usernameArray)
-        return usernameArray
-    }
-
-    function openFileInput() {
-        inputFile = "z-50 absolute visible bg-red-400"
-    }
-
-    async function keyDown(event) {
-        if (event.key == "Enter") {
-            console.log("Enter key pressed")
-            const usernameArrays = performUsernameLogic()
-            showModal = "hidden"
-            showLoading = "my-auto mx-auto loading"
-            console.log(encodeURIComponent(usernameArrays))
-            const response = await fetch(`${prod}twitter?username=${JSON.stringify(encodeURIComponent(usernameArrays))}`, {
-                method: "POST",
-                mode: "no-cors",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(usernameArrays)
-            })
-            console.log(response)
-            const body = await response.json()
-            console.log(body)
-            if (body.includes("Error")) {
-                showModal = "flex flex-col gap-2 w-[400px] h-fit bg-stone-100 mx-auto mt-48 p-6 rounded-lg shadow-2xl"
-                showLoading = "hidden"
-                errorMessage = "mx-auto text-red-400 mt-10 shadow-xl"
-            } else {
-                showModal = "flex flex-col gap-2 w-[400px] h-fit bg-stone-100 mx-auto mt-48 p-6 rounded-lg shadow-2xl"
-                showLoading = "hidden"
-                successMessage = "mx-auto text-green-400 mt-10 shadow-xl"
-            }
+    function performUsernameLogic(csv) {
+        if (csv) {
+            return inputValue
+        } else {
+            const usernameArray = inputValue.split(",")
+            console.log(usernameArray)
+            return usernameArray
         }
     }
 
+    function openFileInput() {
+        if (fileInput) {
+            fileInput.click()
+            console.log("File input button clicked.")
+            inputFile = "hidden z-50 absolute bg-red-400"
+        }
+    }
+
+    function setWIPVisible() {
+        wip = "opacity-100 transition delay-300"
+    }
+
     async function test(event) {
-        if (event.key == "Enter") {
+        if (!inputValue) {
+            noInputValueError = "transition delay-150 text-red-500 mx-auto text-sm"
+            return
+        }
+        if (event.key == "Enter" || event.type === "mousedown") {
             console.log("Enter key pressed")
             showModal = "hidden"
             showLoading = "my-auto mx-auto loading"
-            const usernameArrays = performUsernameLogic()
-            const response = await fetch(`${prod}ai-test?username=${JSON.stringify(encodeURIComponent(usernameArrays))}`, {
+            const usernameArrays = excelOrNot ? performUsernameLogic(true) : performUsernameLogic(false)
+            const response = await fetch(`${prod}/ai-test?username=${JSON.stringify(encodeURIComponent(usernameArrays))}`, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json"
@@ -165,7 +158,7 @@
             labelChartArray.push(lines[0], lines[1], lines[2])
             console.log(labelChartArray)
             if (body.includes("Error")) {
-                showModal = "flex flex-col gap-2 w-[400px] h-fit bg-stone-100 mx-auto mt-48 p-6 rounded-lg shadow-2xl"
+                showModal = "flex flex-col z-50 gap-2 w-[400px] h-fit bg-stone-100 mx-auto mt-48 p-6 rounded-lg shadow-2xl"
                 showLoading = "hidden"
                 errorMessage = "mx-auto text-red-400 mt-10 shadow-xl"
             } else {
@@ -173,7 +166,7 @@
                 //modify chart data
                 chart.data.datasets[0].data = boxChartArray.map(Number)
                 chart.update()
-                showCanvas = "items-center flex flex-row ml-[300px] visible my-auto my-auto w-[60%] h-[450px]"
+                showCanvas = "items-center flex flex-row ml-[30%] visible my-auto my-auto w-[60%] h-[450px]"
                 showLoading = "hidden"
                 successMessage = "mx-auto text-green-400 mt-10 shadow-xl"
             }
@@ -183,8 +176,8 @@
     function performDashboardSearch() {
         console.log(searchValue)
         if (!searchValue) {
-            memeDashboard = "w-full text-sm text-stone-600 transition delay-100 px-4 py-[7px] rounded-lg hover:bg-stone-200 hover:cursor-pointer" //visible
-            trendDashboard = "w-full text-sm text-stone-600 transition delay-100 0 px-4 py-[7px] rounded-lg hover:bg-stone-200 hover:cursor-pointer"
+            memeDashboard = "w-full text-sm text-stone-600 transition delay-100 px-4 py-[7px] rounded-lg flex flex-row justify-between hover:bg-stone-200 hover:cursor-pointer" //visible
+            trendDashboard = "w-full text-sm text-stone-600 transition delay-100 0 px-4 py-[7px] rounded-lg flex flex-row justify-between hover:bg-stone-200 hover:cursor-pointer"
             return
         }
         if (searchValue.toLowerCase().includes("mem")) {
@@ -194,6 +187,37 @@
             memeDashboard = "hidden"
         }
     }
+
+    function processCsv(e) {
+        e.preventDefault()
+        console.log("test")
+        const file = e.target?.files?.[0]
+        console.log(file)
+        if (file) {
+            if (file.type == "text/csv") {
+                console.log("Good file type.")
+                addACSVButton = "CSV succesfully added ✅"
+                const reader = new FileReader()
+                reader.onload = (e) => {
+                    const text = e.target.result
+                    console.log(text)
+                    const splitText = text.split("\n")
+                    splitText.forEach((e, index) => {
+                        splitText[index] = e.replace("\r", "")
+                    })
+                    inputValue = splitText
+                    console.log(inputValue)
+                    excelOrNot = true
+                    //process csv
+                }
+                console.log("Reading text file")
+                reader.readAsText(file)
+            } else {
+                window.alert("Please add a CSV file")
+            }
+        }
+    }
+
 
 </script>
 
@@ -215,6 +239,18 @@
         max-width: 100%;
         height: 400px;
     }
+
+    @keyframes test {
+        0% {
+            transform: translate(0px, 15px);
+        } 100% {
+            transform: translate(1900px, 15px);
+        }
+    }
+
+    .path {
+        animation: test 4s linear infinie;
+    }
 </style>
 
 <main class="flex flex-row bg-white w-full h-screen overflow-scroll">
@@ -222,8 +258,14 @@
     <div class="fixed resizable z-50 bg-white transition delay-100 flex flex-col md:start-0 h-screen py-4 px-2 border-r-[1px] border-stone-300 shadow-2xl"  style={dynamicStyle} bind:this="{resizableDiv}">
         <div class="mx-4 my-2 font-semibold text-2xl hover:cursor-pointer">Analys<span class="gradient">ooo</span>r</div>
         <input class="w-full my-1 p-1 py-[4px] pl-4 mb-4 rounded-lg mx-auto bg-stone-200 shadow-lg" placeholder="🔍 Search" bind:value={searchValue} on:keydown={performDashboardSearch} />
-        <div class={memeDashboard}>Meme predictions</div>
-        <div class={trendDashboard}>Trend predictions</div>
+        <div class={memeDashboard} on:mouseenter={setWIPVisible} on:mouseleave={() => {wip = "hidden"}}>
+            <div>Meme predictions</div>
+            <div class={wip}>🚧</div>
+        </div>
+        <div class={trendDashboard} on:mouseenter={setWIPVisible} on:mouseleave={() => {wip = "hidden"}}>
+            <div>Trend predictions</div>
+            <div class={wip}>🚧</div>
+        </div>
         <div class="resize-cursor" on:mousedown="{startResize}"></div>
     </div>
     <div class={showModal}>
@@ -231,11 +273,14 @@
         <div class="text-xs text-stone-600 mt-[-5px]">The AI model will analyze their latest posts and predict future trends.</div>
         <input class="w-full mt-[4px] p-1 py-[5.5px] pl-[10px] rounded-lg mx-auto bg-stone-200" placeholder="@usernames followed by comma" bind:value={inputValue} on:keydown={test} />
         <div class="text-sm text-stone-700 mx-auto my-1">Or</div>
-        <button class="w-full p-1 py-[5.5px] rounded-lg mx-auto bg-stone-300 font-semibold transition delay-150 hover:bg-blue-100">Add a CSV file</button>
-        <input class={inputFile} type="file" accept=".csv" on:click={openFileInput} />
-        <button class="w-full mt-1 p-1 py-[5.5px] rounded-lg mx-auto bg-stone-200 font-semibold text-blue-500 transition delay-150 hover:bg-blue-100" on:click={test}>Submit</button>
-        <div class={errorMessage}>Internal server error, try again.</div>
-        <div class={successMessage}>Success.</div>
+        <form id="csvform" on:change={processCsv}>
+            <button class="w-full p-1 py-[5.5px] rounded-lg mx-auto bg-stone-300 font-semibold transition delay-150 hover:bg-blue-100" type="button" on:click={openFileInput}>{addACSVButton}</button>
+            <input class={inputFile} type="file" accept=".csv" bind:this={fileInput} />
+        </form>
+        <button class="w-full mt-1 p-1 py-[5.5px] rounded-lg mx-auto bg-stone-200 font-semibold text-blue-500 transition delay-150 hover:bg-blue-100" on:mousedown={test}>Submit</button>
+        <div class={noInputValueError}>Empty fields</div>
+        <div class={errorMessage}>Internal server error, try again</div>
+        <div class={successMessage}>Success</div>
     </div>
     <div class={showCanvas}>
         <div class="flex flex-col w-[150px]">
